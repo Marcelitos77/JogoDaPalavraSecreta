09732d229190a7fb0c2102cd12f509a116ea0d14
Marcelo Siqueira Oliveira <mss.siqueira@gmail.com>
2024-03-03 15:10:16 -0300

    segundo commit com O.O.

--- JogoDaPalavraSecreta/Program.cs
+++ JogoDaPalavraSecreta/Program.cs
@@ -1,47 +1,23 @@
 ﻿using System;
 
-namespace JogoPalavraSecreta
+namespace SegredoPalavra
 {
     class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Digite a palavra secreta:");
-            string palavraSecreta = Console.ReadLine().ToLower();
+            string palavra = Console.ReadLine();
 
             Console.Clear();
 
             Console.WriteLine("Digite a descricao:");
             string descricao = Console.ReadLine();
 
-            int tentativasRestantes = 5;
-
-            Console.Clear();
-            Console.WriteLine($"A descrição é: {descricao}");
-            Console.WriteLine("Você tem 5 tentativas");
-
-            while (tentativasRestantes > 0)
-            {
-                Console.Write("Digite sua resposta: ");
-                string tentativa = Console.ReadLine().ToLower();
-
-                if (tentativa == palavraSecreta)
-                {
-                    Console.Clear();
-                    Console.WriteLine("Parabéns! Você descobriu a palavra!");
-                    Console.WriteLine($"{palavraSecreta}");
-                    return;
-                }
-                else
-                {
-                    tentativasRestantes--;
-                    Console.Clear();
-                    Console.WriteLine($"Resposta incorreta. Tentativas restantes: {tentativasRestantes}");
-                }
-            }
-
-            Console.Clear();
-            Console.WriteLine("Fim do Jogo! Você perdeu!");
+            JogoSegredo jogo = new JogoSegredo(palavra, descricao);
+            jogo.Jogar();
         }
+
     }
+
 }
---
+++ JogoDaPalavraSecreta/SegredoPalavra.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace SegredoPalavra
+{
+    class JogoSegredo
+    {
+        string palavraSecreta;
+        string descricao;
+        int tentativasRestantes;
+
+        public JogoSegredo(string palavra, string desc)
+        {
+            palavraSecreta = palavra.ToLower();
+            descricao = desc;
+            tentativasRestantes = 5;
+        }
+
+        public void Jogar()
+        {
+            Console.Clear();
+            Console.WriteLine($"A descrição é: {descricao}");
+            Console.WriteLine("Você tem 5 tentativas");
+
+            while (tentativasRestantes > 0)
+            {
+                Console.Write("Digite sua resposta: ");
+                string tentativa = Console.ReadLine().ToLower();
+
+                if (tentativa == palavraSecreta)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Parabéns! Você descobriu a palavra!");
+                    Console.WriteLine($"{palavraSecreta}");
+                    return;
+                }
+                else
+                {
+                    tentativasRestantes--;
+                    Console.Clear();
+                    Console.WriteLine($"Resposta incorreta. Tentativas restantes: {tentativasRestantes}");
+                }
+            }
+
+            Console.Clear();
+            Console.WriteLine("Fim do Jogo! Você perdeu!");
+        }
+    }
+}
